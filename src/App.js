03f36e539652e16e10570144f574baa4dd0073ea
vishu-kr <vishwajeetku987@gmail.com>
2021-11-17 01:20:42 +0530

    adding on git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
+import Offers from "./components/Offers.js";
+import Heading from "./components/Heading.js";
+import StarProduct from "./components/StarProduct.js";
+// import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+// import HotAccessories from "./components/HotAccessories.js";
+import ProductReviews from "./components/ProductReviews";
+import Videos from "./components/Videos.js";
+import Banner from "./components/Banner.js";
+import Footer from "./components/Footer.js";
+import NavOptions from "./components/NavOptions.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+
+      {/* <NavOptions
+        miPhones={data.miPhones}
+        redmiPhones={data.redmiPhones}
+        tv={data.tv}
+        laptop={data.laptop}
+        fitnessAndLifeStyle={data.fitnessAndLifeStyle}
+        home={data.home}
+        audio={data.audio}
+        accessories={data.accessories}
+      /> */}
+
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCT" />
+      <StarProduct starProduct={data.starProduct} />
+
+      <Heading text="PRODUCT REVIEWS" />
+
+      <ProductReviews productReviews={data.productReviews} />
+
+      <Heading text="VIDEOS" />
+      <Videos videos={data.videos} />
+      <Heading text="IN THE PRESS" />
+      <Banner banner={data.banner} />
+
+      <Footer footer={data.footer} />
+    </Router>
   );
 }
 
---
+++ src/components/ProductReviewsCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/ProductReviewCard.css"
+
+const ProductReviewsCard = ({image,index,review,name,price}) => {
+  return (
+  <div className="ProductReviewCard">
+      <img src={image} alt={`${index} review`}/>
+      <h5>{review}</h5>
+      <span>{name}</span>
+      <b>{price}</b>
+  </div>);
+};
+
+export default ProductReviewsCard;
